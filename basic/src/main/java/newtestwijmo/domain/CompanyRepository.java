2a0b2e1caac2a92f74f291e215143ff21093b84f
kibumpark0405 <130956306+kibumpark0405@users.noreply.github.com>
2023-08-07 14:01:54 +0900

    commit

---
+++ basic/src/main/java/newtestwijmo/domain/CompanyRepository.java
@@ -0,0 +1,12 @@
+package newtestwijmo.domain;
+
+import newtestwijmo.domain.*;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel="companies", path="companies")
+public interface CompanyRepository extends PagingAndSortingRepository<Company, String>{
+    @Query(value = "select company " +
+        "from Company company " +
+        "where")
+       List<Company> }
